60b7aaaaf3ba0171455a99f9cb1c5143e9fa683e
Michael Frye <michael@michaelfrye.me>
2016-05-23 16:28:00 -0700

    wip on coverting to webpack

---
+++ tools/webpack.config.js
@@ -0,0 +1,101 @@
+/**
+ * React Static Boilerplate
+ * https://github.com/koistya/react-static-boilerplate
+ *
+ * Copyright © 2015-2016 Konstantin Tarkus (@koistya)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+const path = require('path');
+const webpack = require('webpack');
+const extend = require('extend');
+const pkg = require('../package.json');
+var ComponentPlugin = require("component-webpack-plugin");
+
+const isDebug = !(process.argv.includes('--release') || process.argv.includes('-r'));
+const isVerbose = process.argv.includes('--verbose') || process.argv.includes('-v');
+
+/**
+ * Webpack configuration (core/main.js => build/bundle.js)
+ * http://webpack.github.io/docs/configuration.html
+ */
+const config = {
+
+  // The base directory
+  context: path.resolve(__dirname, '../'),
+
+  // The entry point for the bundle
+  entry: ['./lib/index.js'],
+
+  // Options affecting the output of the compilation
+  output: {
+    path: path.resolve(__dirname, '../build'),
+    publicPath: '/',
+    file: 'build/[name].js',
+    sourcePrefix: '  ',
+  },
+
+  // Switch loaders to debug or release mode
+  debug: isDebug,
+
+  // Developer tool to enhance debugging, source maps
+  // http://webpack.github.io/docs/configuration.html#devtool
+  devtool: isDebug ? 'source-map' : false,
+
+  // What information should be printed to the console
+  stats: {
+    colors: true,
+    reasons: isDebug,
+    hash: isVerbose,
+    version: isVerbose,
+    timings: true,
+    chunks: isVerbose,
+    chunkModules: isVerbose,
+    cached: isVerbose,
+    cachedAssets: isVerbose,
+  },
+
+  // The list of plugins for Webpack compiler
+  plugins: [
+    new ComponentPlugin({}, ['components']),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': isDebug ? '"development"' : '"production"',
+      __DEV__: isDebug,
+    }),
+  ],
+
+  // Options affecting the normal modules
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, '../lib'),
+        ],
+        loader: 'babel-loader',
+        query: extend({}, pkg.babel, { babelrc: false }),
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+        loader: 'url-loader?limit=10000',
+      }
+    ],
+  }
+
+};
+
+// Optimize the bundle in release (production) mode
+if (!isDebug) {
+  config.plugins.push(new webpack.optimize.DedupePlugin());
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({ compress: { warnings: isVerbose } }));
+  config.plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+}
+
+module.exports = config;
